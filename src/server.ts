da9029721979a08d8003897da7b4c0c020f30e72
Thiago Pizzutti <tpizzutti@gmail.com>
2020-08-06 16:17:49 -0300

    feat: setup server

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,12 @@
 import express from 'express'
+import routes from './routes'
+import Freeman from 'morgan'
 
 const app = express()
+app.use(Freeman('dev'))
+app.use(express.json())
+app.use(routes)
 
-app.get('/users', (request, response) => {
-  const users = [
-    {
-      name: "Pedro",
-      age: 37
-    }
-  ]
-  
-  
-  return response.json(users)
-})
-
-
-app.listen(3333)
+app.listen(3333, () =>
+  console.log("Hello Mother Fuckers🚀💥🚀💥🚀💥🚀💥🚀💥🚀")
+)
